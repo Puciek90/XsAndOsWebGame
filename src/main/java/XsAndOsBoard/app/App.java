251227f4984c6eb88e2824dba19e4d923ca3a47a
Mateusz <mateusz.pluciennik93@gmail.com>
2017-01-19 22:13:57 +0100

    Just cleaned main function ;)

--- src/main/java/XsAndOsBoard/app/App.java
+++ src/main/java/XsAndOsBoard/app/App.java
@@ -1,16 +1,9 @@
 package XsAndOsBoard.app;
 
-import XsAndOsBoard.board.Board;
-import XsAndOsBoard.gameControll.GameController;
-
-
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        Board board = new Board(3,5);
-        GameController gameController = new GameController(board, 3);
-        gameController.help();
-        board.showBoard();
+        System.out.println("Hello world! :)");
     }
 }
