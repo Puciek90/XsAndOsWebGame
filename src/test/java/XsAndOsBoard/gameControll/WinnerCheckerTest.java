a2b8fec777015682994f193dae9663873f4a0289
Mateusz <mateusz.pluciennik93@gmail.com>
2017-01-19 18:54:56 +0100

    Refactored condition in findLeftDownFieldFromLastMoveField and findLeftTopFieldFromLastMoveField functions

--- src/test/java/XsAndOsBoard/gameControll/WinnerCheckerTest.java
+++ src/test/java/XsAndOsBoard/gameControll/WinnerCheckerTest.java
@@ -1,11 +1,186 @@
 package XsAndOsBoard.gameControll;
 
+import XsAndOsBoard.board.Board;
+import XsAndOsBoard.board.Field;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 // Created by Mateusz Płuciennik on 17.01.17.
 
-public class GameControllerTest {
+public class WinnerCheckerTest {
     @Test
-    public void help() throws Exception {
+    public void shouldFindWinnerInRow() {
+        //given
+        Field lastMovieField = new Field(0,0);
+        Board board = new Board(1,3);
+        board.setLastMoveField(lastMovieField);
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkHorizontal().equals("0"));
+    }
+
+    @Test
+    public void shouldFindWinnerInRow2() {
+        //given
+        Field lastMoveField = new Field(2,0);
+        Board board = new Board(1,5);
+        board.setLastMoveField(lastMoveField);
+        board.setFieldSymbol(new Field(2,0), "Z");
+        board.setFieldSymbol(new Field(3,0), "Z");
+        board.setFieldSymbol(new Field(4,0), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkHorizontal().equals("Z"));
+    }
+
+    @Test
+    public void shouldFindWinnerInRow3() {
+        //given
+        Field lastMoveField = new Field(2,0);
+        Board board = new Board(1,6);
+
+        board.setLastMoveField(lastMoveField);
+        board.setFieldSymbol(new Field(1,0), "Z");
+        board.setFieldSymbol(new Field(2,0), "Z");
+        board.setFieldSymbol(new Field(3,0), "Z");
+        board.setFieldSymbol(new Field(4,0), "Z");
+        board.setFieldSymbol(new Field(5,0), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 5);
+
+        //then
+        assertTrue(winnerChecker.checkHorizontal().equals("Z"));
+    }
+
+
+    @Test
+    public void shouldFindWinnerInColumn(){
+        //given
+        Field lastMovieField = new Field(0,0);
+        Board board = new Board(3,1);
+        board.setLastMoveField(lastMovieField);
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkVertical().equals("0"));
+    }
+
+    @Test
+    public void shouldFindWinnerInColumn2(){
+        //given
+        Field lastMoveField = new Field(0,2);
+        Board board = new Board(5,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(0,2), "Z");
+        board.setFieldSymbol(new Field(0,3), "Z");
+        board.setFieldSymbol(new Field(0,4), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkVertical().equals("Z"));
+    }
+
+    @Test
+    public void shouldFindWinnerInColumn3(){
+        //given
+        Field lastMoveField = new Field(0,3);
+        Board board = new Board(6,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(0,1), "Z");
+        board.setFieldSymbol(new Field(0,2), "Z");
+        board.setFieldSymbol(new Field(0,3), "Z");
+        board.setFieldSymbol(new Field(0,4), "Z");
+        board.setFieldSymbol(new Field(0,5), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 5);
+
+        //then
+        assertTrue(winnerChecker.checkVertical().equals("Z"));
+    }
+
+    @Test
+    public void shouldNotFindWinnerInColumn(){
+        //given
+        Field lastMoveField = new Field(0,2);
+        Board board = new Board(5,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(0,2), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkHorizontal().equals(""));
+    }
+
+    @Test
+    public void shouldNotFindWinnerInRow(){
+        //given
+        Field lastMoveField = new Field(2,0);
+        Board board = new Board(1,5);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(2,0), "Z");
+
+        //when
+        WinnerChecker winnerChecker = new WinnerChecker(board, 3);
+
+        //then
+        assertTrue(winnerChecker.checkHorizontal().equals(""));
+    }
+
+    // TODO: 19.01.17 Change function name! Present name mislead.
+    // TODO: 19.01.17 test more points without code duplication 
+    @Test
+    public void shouldFindLeftBottomCornerFromLastMoveField(){
+        //given
+        Board board = new Board(10,10);
+        Field lastMoveField = new Field(5,3);
+        board.setLastMoveField(lastMoveField);
+
+        WinnerChecker winnerChecker = new WinnerChecker(board,3);
+
+        //when
+        Field field = winnerChecker.findLeftDownFieldFromLastMoveField();
+
+        //then
+        assertTrue(field.getFieldXCoordinate()==0);
+        assertTrue(field.getFieldYCoordinate()==8);
+
+    }
+
+    // TODO: 19.01.17 Change function name! Present name mislead.
+    // TODO: 19.01.17 test more points without code duplication
+    @Test
+    public void shouldFindTopFromLastMoveField(){
+        //given
+        Board board = new Board(10,10);
+        Field lastMoveField = new Field(8,10);
+        board.setLastMoveField(lastMoveField);
+
+        WinnerChecker winnerChecker = new WinnerChecker(board,3);
+
+        //when
+        Field field = winnerChecker.findLeftTopFieldFromLastMoveField();
+
+        //then
+        assertTrue(field.getFieldXCoordinate()==0);
+        assertTrue(field.getFieldYCoordinate()==2);
 
     }
 
