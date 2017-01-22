f690f7b46565ca9cf02901f198b952fe7c9e62cc
Mateusz <mateusz.pluciennik93@gmail.com>
2017-01-22 17:29:56 +0100

    Added WinnerCheckerTest

--- src/test/java/XsAndOsBoard/gameControll/winnerChcecker/WinnerCheckerTest.java
+++ src/test/java/XsAndOsBoard/gameControll/winnerChcecker/WinnerCheckerTest.java
@@ -9,23 +9,75 @@ import static org.junit.Assert.assertTrue;
 // Created by Mateusz Płuciennik on 17.01.17.
 
 public class WinnerCheckerTest {
-    // TODO: 21.01.17 test for every scenario
     @Test
     public void shouldGiveWinnerSymbolWhenWinnerIsInRow(){
         //given
+        String symbol = "X";
         Board board = new Board(3,3);
         Field lastMoveField = new Field(1,1);
         board.setLastMoveField(lastMoveField);
 
-        board.setFieldSymbol(new Field(0,1), "Z");
-        board.setFieldSymbol(new Field(1,1), "Z");
-        board.setFieldSymbol(new Field(2,1), "Z");
-
-        board.showBoard();
+        board.setFieldSymbol(new Field(0,1), symbol);
+        board.setFieldSymbol(new Field(1,1), symbol);
+        board.setFieldSymbol(new Field(2,1), symbol);
 
         WinnerChecker winnerChecker = new WinnerChecker(board,3);
-
+        //when
         //then
-        assertTrue(winnerChecker.giveWinnersSymbolIfWinnerExist().equals("Z"));
+        assertTrue(winnerChecker.giveWinnersSymbolIfWinnerExist().equals(symbol));
+    }
+
+    @Test
+    public void shouldGiveWinnerSymbolWhenWinnerIsInColumn(){
+        //given
+        String symbol = "X";
+        Board board = new Board(3,3);
+        Field lastMoveField = new Field(1,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(1,0), symbol);
+        board.setFieldSymbol(new Field(1,1), symbol);
+        board.setFieldSymbol(new Field(1,2), symbol);
+
+        WinnerChecker winnerChecker = new WinnerChecker(board,3);
+        //when
+        //then
+        assertTrue(winnerChecker.giveWinnersSymbolIfWinnerExist().equals(symbol));
+    }
+
+    @Test
+    public void shouldGiveWinnerSymbolWhenWinnerIsInRightUpSlant(){
+        //given
+        String symbol = "X";
+        Board board = new Board(3,3);
+        Field lastMoveField = new Field(1,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(0,0), symbol);
+        board.setFieldSymbol(new Field(1,1), symbol);
+        board.setFieldSymbol(new Field(2,2), symbol);
+
+        WinnerChecker winnerChecker = new WinnerChecker(board,3);
+        //when
+        //then
+        assertTrue(winnerChecker.giveWinnersSymbolIfWinnerExist().equals(symbol));
+    }
+
+    @Test
+    public void shouldGiveWinnerSymbolWhenWinnerIsInRightDownSlant(){
+        //given
+        String symbol = "X";
+        Board board = new Board(3,3);
+        Field lastMoveField = new Field(1,1);
+        board.setLastMoveField(lastMoveField);
+
+        board.setFieldSymbol(new Field(0,2), symbol);
+        board.setFieldSymbol(new Field(1,1), symbol);
+        board.setFieldSymbol(new Field(2,0), symbol);
+
+        WinnerChecker winnerChecker = new WinnerChecker(board,3);
+        //when
+        //then
+        assertTrue(winnerChecker.giveWinnersSymbolIfWinnerExist().equals(symbol));
     }
 }
